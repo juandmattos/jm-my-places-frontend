86de2563ddbc1bc7f3eb2c501c0a24005de5f90a
Juan Diego Mattos <juandmattos@gmail.com>
2020-04-13 15:01:08 -0300

    Add auth-hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React, { useState, useCallback, useEffect } from 'react'
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom'
 
 import Users from './user/pages/Users'
 import NewPlace from './places/pages/NewPlace'
@@ -8,55 +13,10 @@ import UpdatePlace from './places/pages/UpdatePlace'
 import Auth from './user/pages/Auth'
 import MainNavigation from './shared/components/Navigation/MainNavigation'
 import { AuthContext } from './shared/context/auth-context'
-
-let logoutTimer
+import { useAuth } from './shared/hooks/auth-hook'
 
 const App = () => {
-  const [token, setToken] = useState(false)
-  const [userId, setUserId] = useState(false)
-  const [tokenExpirationDate, setTokenExpirationDate] = useState()
-
-  const login = useCallback((userId, token, expirationDate) => {
-    setToken(token)
-    setUserId(userId)
-    const expDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60) // current date + 1 hour
-    setTokenExpirationDate(expDate)
-    localStorage.setItem(
-      'userData',
-      JSON.stringify({
-        expiration: expDate.toISOString(),
-        token: token,
-        userId: userId
-      })
-    )
-  }, [])
-
-  const logout = useCallback(() => {
-    setToken(null)
-    setTokenExpirationDate(null)
-    setUserId(null)
-    localStorage.removeItem('userData')
-  }, [])
-
-  useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem('userData'))
-    if (
-      storedData &&
-      storedData.token &&
-      new Date(storedData.expiration) > new Date()
-    ) {
-      login(storedData.userId, storedData.token, new Date(storedData.expiration))
-    }
-  }, [login]) // After the render cycle. With [] only once after it mounts
-
-  useEffect(() => {
-    if (token && tokenExpirationDate) {
-      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime()
-      logoutTimer = setTimeout(logout, remainingTime)
-    } else {
-      clearTimeout(logoutTimer)
-    }
-  }, [token, logout, tokenExpirationDate])
+  const { token, login, logout, userId } = useAuth()
 
   let routes
 
@@ -72,7 +32,7 @@ const App = () => {
         <Route path='/places/new' exact>
           <NewPlace />
         </Route>
-        <Route path='/places/:placeId' exact>
+        <Route path='/places/:placeId'>
           <UpdatePlace />
         </Route>
         <Redirect to='/' />
@@ -107,9 +67,7 @@ const App = () => {
     >
       <Router>
         <MainNavigation />
-        <main>
-          {routes}
-        </main>
+        <main>{routes}</main>
       </Router>
     </AuthContext.Provider>
   )
