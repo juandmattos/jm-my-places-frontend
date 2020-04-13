755a01dc8a321a934b1568b3ffee57f2a929089f
Juan Diego Mattos <juandmattos@gmail.com>
2020-04-13 10:58:26 -0300

    Add token to frontend and use it in new place

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -52,7 +52,14 @@ const NewPlace = () => {
       formData.append('address', formState.inputs.address.value)
       formData.append('creator', auth.userId)
       formData.append('image', formState.inputs.image.value)
-      await sendRequest(`${REACT_APP_PLACES_URL}/api/places`, 'POST', formData)
+      await sendRequest(
+        `${REACT_APP_PLACES_URL}/api/places`,
+        'POST',
+        formData,
+        {
+          Authorization: 'Bearer ' + auth.token
+        }
+      )
       history.push('/')
     } catch (err) {}
   }
